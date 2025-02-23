609acd8736c970f77ad52219bb0bb14df43eee2d
EstarlinJimenez <estarlinalexanderjimenezgarcia@gmail.com>
2025-02-23 00:39:10 -0400

    programa 2.10

---
+++ programa 2.10/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* par, impar o nulo.
+
+NUM:variable de tipo entero. */
+
+void main(void)
+{
+int NUM;
+printf("Ingrese el numero: ");
+scanf("%d", %NUM);
+if (NUM == 0)
+    printf("\nNulo") > 0)
+  else
+    printf("\nImpar");
+}
